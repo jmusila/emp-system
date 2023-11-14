2b7404085c9de3a77a2764f5ab75167d5188d0ac
jmusila <jonathanmusila6@gmail.com>
2023-11-14 09:48:04 +0300

    added spatie for roles/permissions

---
+++ database/migrations/2023_12_12_000000_create_users_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('othername')->nullable();
+            $table->string('surname');
+            $table->string('title')->nullable();
+            $table->date('dob')->nullable();
+            $table->string('id_number')->nullable();
+            $table->string('gender')->nullable();
+            $table->string('nationality')->nullable();
+            $table->foreignIdFor('ethnicities')->nullable();
+            $table->foreignIdFor('counties')->nullable();
+            $table->string('address')->nullable();
+            $table->string('code')->nullable();
+            $table->string('telephone_number')->unique()->nullable();
+            $table->string('mobile_number')->unique();
+            $table->string('email')->unique();
+            $table->string('alternative_contact_person')->unique();
+            $table->string('living_with_disability')->nullable();
+            $table->string('nature_of_disability')->nullable();
+            $table->string('disability_reg_no')->nullable();
+            $table->string('disability_reg_date')->nullable();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
