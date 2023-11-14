bd8fb0dd08ac6fdda5f507e02f60188f818d93c7
jmusila <jonathanmusila6@gmail.com>
2023-11-14 09:55:01 +0300

    load roles/permissions

---
+++ app/Console/Commands/InitializeTestEnvironment.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\DB;
+
+class InitializeTestEnvironment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:initialize-test-environment';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Initialize test environment';
+
+    public function handle(): void
+    {
+        if (App::environment() === 'production') {
+            throw new Exception('this command must never be executed in a NON development environment');
+        }
+
+        $this->call('migrate:fresh', ['--seed' => false], $this->output);
+
+        $this->info("\n\nLoading Roles...");
+        $this->call('roles:load', [], $this->output);
+
+        $this->info("\n\nSeeding Data");
+        $this->call('db:seed');
+
+        $this->call('passport:install', [], $this->output);
+    }
+}
---
+++ app/Enums/Role.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Enums;
+
+class Roles
+{
+    public const ADMIN = "Admin";
+
+    public const HUMAN_RESOURCE = "HumanResource";
+
+    public const FIELD_OFFICER = "FieldOfficer";
+}
